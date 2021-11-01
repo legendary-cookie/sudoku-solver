8f93da79deca2896c5ce5c4b3c9c4365c90cbc42
legendary-cookie <github.com@xolley.de>
2021-11-01 18:15:22 +0100

    sudoku2c script

---
+++ scripts/sudk2c.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+input=$1
+# 9x9, change to set different
+code="int puzzle[9][9]={"
+
+
+function text2c () {
+	text=$1
+	row="{"
+	for (( i=0;i<${#text}; i++ )); do
+		char="${text:$i:1}"
+		row+="$char,"
+	done
+	row=$(echo ${row%,*} ${row##*,})
+	row+="},"
+	code=$code$row
+}
+
+
+while read -r line
+do
+	if [[ ! -z "$line" ]]; then
+		text2c $(echo "$line" | sed 's/ //g')
+	fi
+done < "$input"
+
+code=$(echo ${code%,*} ${code##*,})
+code+="}"
+
+echo $code | sed 's/x/0/g'
+
+
